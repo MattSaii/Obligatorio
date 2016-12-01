469acb01e57151d16ecce815b1a48d5f73cfc651
Paablo15 <pisp06@gmail.com>
2016-11-30 23:39:53 -0300

    Ponele

--- ObligatorioAlgoritmos/src/obligatorioalgoritmos/ObligatorioAlgoritmos.java
+++ ObligatorioAlgoritmos/src/obligatorioalgoritmos/ObligatorioAlgoritmos.java
@@ -4,10 +4,10 @@ public class ObligatorioAlgoritmos {
 
     public static void main(String[] args) {
         // TODO code application logic here
-         Sistema sis= new Sistema();
+        Sistema sis= new Sistema();
         sis.inicializarSistema(6);
         sis.registrarEmpresa("Empresa1", "Cerca", "Uru", "email@cas.com", "blue");
-        
+        sis.listadoEmpresas();
     }
     
 }
